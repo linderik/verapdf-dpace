ec8fbf02b40d5eafc66d2dc6f32dc4358fedc0a6
linderik <erik.lindholm@helsinki.fi>
2017-11-22 14:21:12 +0200

    - reformating code

--- f_inet.py
+++ f_inet.py
@@ -14,57 +14,53 @@ module_logger = logging.getLogger('harvest.inet')
 
 
 def be_nice():
-	# Sleep.
-	# p_secs = Seconds to sleep
-	
-	time.sleep(WAIT_SECS)
-	
-	return
+    # Sleep.
+    # p_secs = Seconds to sleep
+
+    time.sleep(WAIT_SECS)
+
+    return
 
 
 def get_url(p_url):
-	# Tries to fetch URL.
-	# p_url = URL to get
-	# p_wait = Time to wait after request.
+    # Tries to fetch URL.
+    # p_url = URL to get
+    # p_wait = Time to wait after request.
 
-	socket.setdefaulttimeout(300)
-	
-	headers = { 'User-Agent' : 'Mozilla/5.0' }
+    socket.setdefaulttimeout(300)
 
-	url = p_url
-	try:
-		url = url.decode("utf-8", "strict")
-		#print('success')
-	except UnicodeEncodeError, e:
-		url = unicodedata.normalize('NFC', url).encode('utf-8')
-		#print('fail')
-	request = urllib2.Request(url, None, headers)
-	data = None
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-	try:
-		response = urllib2.urlopen(request)
-	except urllib2.HTTPError, e:
-		module_logger.error('HTTPError: ' + str(e) + ' URL: ' + p_url)
-	except urllib2.URLError, e:
-		module_logger.error('URLError: ' + str(e) + ' URL: ' + p_url)
-	except httplib.BadStatusLine, e:
-		module_logger.error('BadStatusLine: ' + str(e) + ' URL: ' + p_url)
-	except UnicodeDecodeError, e:
-		module_logger.error('UnicodeDecodeError: ' + str(e) + ' URL: ' + p_url)
-	else:
-		# check for redirects to login, dont mind about ADDITION OF some querystring
-		rd_check = response.geturl()
-		#if (((url == rd_check) or (rd_check2 in url)) and (rd_check.count("login") == 0)):
+    url = p_url
+    try:
+        url = url.decode("utf-8", "strict")
+    # print('success')
+    except UnicodeEncodeError, e:
+        url = unicodedata.normalize('NFC', url).encode('utf-8')
+    # print('fail')
+    request = urllib2.Request(url, None, headers)
+    data = None
 
-		if (rd_check.count("login") == 0):
-			data = response.read()
-		else:
-			module_logger.info('Login detected: ' + rd_check + ' URL: ' + p_url)
-		
-	be_nice()
+    try:
+        response = urllib2.urlopen(request)
+    except urllib2.HTTPError, e:
+        module_logger.error('HTTPError: ' + str(e) + ' URL: ' + p_url)
+    except urllib2.URLError, e:
+        module_logger.error('URLError: ' + str(e) + ' URL: ' + p_url)
+    except httplib.BadStatusLine, e:
+        module_logger.error('BadStatusLine: ' + str(e) + ' URL: ' + p_url)
+    except UnicodeDecodeError, e:
+        module_logger.error('UnicodeDecodeError: ' + str(e) + ' URL: ' + p_url)
+    else:
+        # check for redirects to login, dont mind about ADDITION OF some querystring
+        rd_check = response.geturl()
+        # if (((url == rd_check) or (rd_check2 in url)) and (rd_check.count("login") == 0)):
 
-	return data
-	
-	
+        if (rd_check.count("login") == 0):
+            data = response.read()
+        else:
+            module_logger.info('Login detected: ' + rd_check + ' URL: ' + p_url)
 
+    be_nice()
 
+    return data
--- f_string.py
+++ f_string.py
@@ -11,224 +11,218 @@ module_logger = logging.getLogger('harvest.string')
 
 
 def htc(m):
-	# used by urldecode
-	return chr(int(m.group(1),16))
+    # used by urldecode
+    return chr(int(m.group(1), 16))
 
 
 def id_to_pathname(p_str):
-	# Converts an ID string to a suitable dir name.
-	# Currently just converts some characters to underscores.
-	# p_str = ID string
-	
-	normalized = p_str.replace(':', '_')
-	normalized = normalized.replace('/', '_')
-	normalized = normalized.replace(' ', '_')
+    # Converts an ID string to a suitable dir name.
+    # Currently just converts some characters to underscores.
+    # p_str = ID string
 
-	return normalized
+    normalized = p_str.replace(':', '_')
+    normalized = normalized.replace('/', '_')
+    normalized = normalized.replace(' ', '_')
+
+    return normalized
 
 
 def id_to_pathname_long(p_str):
-	# Converts an ID string to a suitable dir name.
-	# Currently just converts some characters to underscores.
-	# p_str = ID string
-	
-	normalized = p_str.replace(':', '_')
-	normalized = normalized.replace('/', '_')
-	normalized = normalized.replace(' ', '_')
-	normalized = normalized[-1:] + '/' + normalized
+    # Converts an ID string to a suitable dir name.
+    # Currently just converts some characters to underscores.
+    # p_str = ID string
 
-	return normalized
+    normalized = p_str.replace(':', '_')
+    normalized = normalized.replace('/', '_')
+    normalized = normalized.replace(' ', '_')
+    normalized = normalized[-1:] + '/' + normalized
 
-	
+    return normalized
+
+
 def l_remove_containing(p_list, p_str):
-	# Returns list of strings that don't contain a specified substring.
-	# p_list = original list
-	# p_str = substring
-	
-	result = []
-	
-	for tstr in p_list:	
-		if (tstr.count(p_str) > 0):
-			pass
-		else:
-			result.append(tstr)
+    # Returns list of strings that don't contain a specified substring.
+    # p_list = original list
+    # p_str = substring
 
-	return result
+    result = []
+
+    for tstr in p_list:
+        if (tstr.count(p_str) > 0):
+            pass
+        else:
+            result.append(tstr)
+
+    return result
 
 
 def l_remove_endswith(p_list, p_str):
-	# Returns list of strings, that don't end with specified string.
-	# p_list = original list
-	# p_str = string to check
-	
-	result = []
-	
-	for tstr in p_list:	
-		if (tstr.endswith(p_str)):
-			pass
-		else:
-			result.append(tstr)
+    # Returns list of strings, that don't end with specified string.
+    # p_list = original list
+    # p_str = string to check
 
-	return result
+    result = []
 
-	
+    for tstr in p_list:
+        if (tstr.endswith(p_str)):
+            pass
+        else:
+            result.append(tstr)
+
+    return result
+
+
 def l_remove_not_containing(p_list, p_str):
-	# Returns list of strings that contain a specified substring.
-	# p_list = original list
-	# p_str = substring
-	
-	result = []
-	
-	for tstr in p_list:	
-		if (tstr.count(p_str) > 0):
-			result.append(tstr)
+    # Returns list of strings that contain a specified substring.
+    # p_list = original list
+    # p_str = substring
 
-	return result
+    result = []
+
+    for tstr in p_list:
+        if (tstr.count(p_str) > 0):
+            result.append(tstr)
+
+    return result
 
 
 def l_remove_startswith(p_list, p_str):
-	# Returns list of strings, that don't start with specified string.
-	# p_list = original list
-	# p_str = string to check
-	
-	result = []
-	
-	for tstr in p_list:	
-		if (tstr.startswith(p_str)):
-			pass
-		else:
-			result.append(tstr)
+    # Returns list of strings, that don't start with specified string.
+    # p_list = original list
+    # p_str = string to check
 
-	return result
-	
-	
+    result = []
+
+    for tstr in p_list:
+        if (tstr.startswith(p_str)):
+            pass
+        else:
+            result.append(tstr)
+
+    return result
+
+
 def l_str_before_last(p_list, p_delimiter):
-	# Returns list of pathnames, truncated at a specified delimiter.
-	# p_list = a list of paths
-	# p_delimiter = string is split at the first occurence of this character
-	
-	result = []
-	
-	for tstr in p_list:	
-		if (tstr.count(p_delimiter) > 0):
-			res, dummy = tstr.split(p_delimiter, 1)
-		else:
-			res = tstr
-		
-		result.append(res)
+    # Returns list of pathnames, truncated at a specified delimiter.
+    # p_list = a list of paths
+    # p_delimiter = string is split at the first occurence of this character
 
-	return result
+    result = []
 
-	
+    for tstr in p_list:
+        if (tstr.count(p_delimiter) > 0):
+            res, dummy = tstr.split(p_delimiter, 1)
+        else:
+            res = tstr
+
+        result.append(res)
+
+    return result
+
+
 def list2str(p_list):
-	# Converts list to comma separated string.
-	# p_list = list to convert
-	
-	result = ""
-	
-	if (len(p_list)>0):
+    # Converts list to comma separated string.
+    # p_list = list to convert
 
-		c = 1
-		for tstr in p_list:	
-		
-			result = result + tstr
-			if (c < len(p_list)):
-				result = result + ", "
-			c = c + 1
-	
-	return result
-	
+    result = ""
+
+    if (len(p_list) > 0):
+
+        c = 1
+        for tstr in p_list:
+
+            result = result + tstr
+            if c < len(p_list):
+                result = result + ", "
+            c = c + 1
+
+    return result
+
 
 def parse_timestring(p_str):
-	# Converts strings to time.	
-	# p_str = Time as "YYYY-MM-DDThh:mm:ss" or "YYYY-MM-DD"
-	
-	t = None
+    # Converts strings to time.
+    # p_str = Time as "YYYY-MM-DDThh:mm:ss" or "YYYY-MM-DD"
 
-	try:
-		t = time.strptime(p_str[:19], "%Y-%m-%dT%H:%M:%S")
-	except ValueError, e:
-		try:
-			t = time.strptime(p_str[:10], "%Y-%m-%d")
-		except ValueError, e:
-			t = None
-	
-	return t
+    t = None
+
+    try:
+        t = time.strptime(p_str[:19], "%Y-%m-%dT%H:%M:%S")
+    except ValueError, e:
+        try:
+            t = time.strptime(p_str[:10], "%Y-%m-%d")
+        except ValueError, e:
+            t = None
+
+    return t
 
 
 def str_after_last(p_str, p_delimiter):
-	# Get substring after delimiter. If delimiter is not found, return whole string.
-	# p_str = original string
-	# p_delimiter = delimiter string
+    # Get substring after delimiter. If delimiter is not found, return whole string.
+    # p_str = original string
+    # p_delimiter = delimiter string
 
-	result = p_str
-	
-	if p_str.count(p_delimiter):
-		dummy, result = p_str.rsplit(p_delimiter, 1)
+    result = p_str
 
-	return result
+    if p_str.count(p_delimiter):
+        dummy, result = p_str.rsplit(p_delimiter, 1)
+
+    return result
 
 
 def str_before_last(p_str, p_delimiter):
-	# Get substring before delimiter. If delimiter is not found, return whole string.
-	# p_str = original string
-	# p_delimiter = delimiter string
+    # Get substring before delimiter. If delimiter is not found, return whole string.
+    # p_str = original string
+    # p_delimiter = delimiter string
 
-	result = p_str
-	
-	if p_str.count(p_delimiter):
-		result, dummy = p_str.rsplit(p_delimiter, 1)
+    result = p_str
 
-	return result
+    if p_str.count(p_delimiter):
+        result, dummy = p_str.rsplit(p_delimiter, 1)
+
+    return result
 
 
 def str2url(p_str):
-	# URLencode a string
-	# p_str = string to encode
-	
-	result = ""
-	result = urllib.url2pathname(p_str)
-	
-	return result
+    # URLencode a string
+    # p_str = string to encode
+
+    result = ""
+    result = urllib.url2pathname(p_str)
+
+    return result
 
 
 def url2filename(p_url):
-	# URLencoding + UTF-strangeness turned to a neat filename.
-	
-	result = ""
-	
-	result = str_after_last(p_url, "/")
-	result = str_before_last(result, "?")
-	
-	try:
-		result = urldecode(result)
-	except UnicodeDecodeError, e:
-		result = str2url(result)
-		result = unicodedata.normalize('NFC', result).encode('utf-8')
+    # URLencoding + UTF-strangeness turned to a neat filename.
 
-	try:
-		result = result.decode("utf-8", "ignore")
-	except UnicodeEncodeError, e:
-		#result = str2url(result)
-		result = unicodedata.normalize('NFC', result).encode('utf-8')
-		#result = result.decode("iso-8859-1", "ignore")
-		result = result.decode("utf-8", "ignore")
-		
-		print result
-		
-	result = id_to_pathname(result)
-	
-	return result
-	
+    result = ""
+
+    result = str_after_last(p_url, "/")
+    result = str_before_last(result, "?")
+
+    try:
+        result = urldecode(result)
+    except UnicodeDecodeError, e:
+        result = str2url(result)
+        result = unicodedata.normalize('NFC', result).encode('utf-8')
+
+    try:
+        result = result.decode("utf-8", "ignore")
+    except UnicodeEncodeError, e:
+        # result = str2url(result)
+        result = unicodedata.normalize('NFC', result).encode('utf-8')
+        # result = result.decode("iso-8859-1", "ignore")
+        result = result.decode("utf-8", "ignore")
+
+        print result
+
+    result = id_to_pathname(result)
+
+    return result
+
 
 def urldecode(url):
-	# found from internets..
-	
-	rex=re.compile('%([0-9a-hA-H][0-9a-hA-H])',re.M)
-	return rex.sub(htc,url)
+    # found from internets..
 
-
-	
-
-
-
+    rex = re.compile('%([0-9a-hA-H][0-9a-hA-H])', re.M)
+    return rex.sub(htc, url)
